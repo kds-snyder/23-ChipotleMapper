3751b922fa7aefd4a35717e83143d8f29aee7e57
kds-snyder <kds_snyder@yahoo.com>
2015-10-27 18:37:41 -0700

    Fixes to ensure that input zip code does not change message of displayed locations until it is searched

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,7 +1,8 @@
 angular.module('app').controller('ChipotleMapController', function($scope, ChipotleService) {
 
   $scope.chipotleData = [];
-  $scope.zipcode = '';
+  $scope.displayZipcode = '';    // zipcode for displaying locations found
+  $scope.inputZipcode = ''; // input zipcode
   $scope.map = {
     center: {
       latitude: 40,
@@ -23,20 +24,22 @@ angular.module('app').controller('ChipotleMapController', function($scope, Chipo
   // Search for locations with zipcode and place in chipotleData
   // Center the map using the latitude and longitude of the first location
   $scope.searchLocations = function () {
-
-    ChipotleService.getLocations($scope.zipcode).then(function(data) {
-      // function might return no data, or data that does not contain address objects
+    ChipotleService.getLocations($scope.inputZipcode).then(function(data) {
+      // No locations were found if function returned no data,
+      //   or data that does not contain address objects
       if (data.length > 0 && (typeof data[0].address != "undefined"))  {
           $scope.chipotleData=data;
+          $scope.displayZipcode = $scope.inputZipcode; // set after search, for correct display of number of locations
           $scope.map.center = {
             latitude: $scope.chipotleData[0].address.latitude,
             longitude: $scope.chipotleData[0].address.longitude
           };
-
       }
       else {
         // initialize search data if  no locations found
+        $scope.displayZipcode = $scope.inputZipcode; // set after search, for correct display of number of locations
         $scope.initializeSearch();
+
       }
 
     });
